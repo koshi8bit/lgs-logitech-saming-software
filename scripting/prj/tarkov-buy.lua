31268a7b7b77a221814c3943e2acde15ab2ef47f
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2026-01-08 01:33:12 +0700

    path

--- scripting/prj/tarkov-buy.lua
+++ scripting/prj/tarkov-buy.lua
@@ -1,4 +1,4 @@
--- data = dofile[[F:\home\koshi8bit\soft\windows\01-main\logitech\scripting\lua\tarkov-buy.lua]]
+-- data = dofile[[F:\home\koshi8bit\soft\windows\01-main\logitech\scripting\prj\tarkov-buy.lua]]
 function OnEvent(event, arg)
 	OutputLogMessage("event = %s, arg = %s\n", event, arg);
 	if (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
